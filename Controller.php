64f157a2cf2d24df420c926c5c019a7e5d410ab5
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-02-12 21:43:10 +0100

    Bulkl path fix reverse

---
+++ Controller.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\ResearchDevelopment;
+
+use Modules\Navigation\Models\Navigation;
+use Modules\Navigation\Views\NavigationView;
+use phpOMS\Contract\RenderableInterface;
+use phpOMS\Message\RequestAbstract;
+use phpOMS\Message\ResponseAbstract;
+use phpOMS\Module\ModuleAbstract;
+use phpOMS\Module\WebInterface;
+use phpOMS\Views\View;
+use phpOMS\Views\ViewLayout;
+
+/**
+ * Research & Development controller class.
+ *
+ * @category   Modules
+ * @package    Modules\ResearchDevelopment
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+class Controller extends ModuleAbstract implements WebInterface
+{
+
+    /**
+     * Module path.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    /* public */ const MODULE_PATH = __DIR__;
+
+    /**
+     * Module version.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    /* public */ const MODULE_VERSION = '1.0.0';
+
+    /**
+     * Module name.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    /* public */ const MODULE_NAME = 'ResearchDevelopment';
+
+    /**
+     * Providing.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $providing = [];
+
+    /**
+     * Dependencies.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static $dependencies = [
+    ];
+
+    /**
+     * @param RequestAbstract  $request  Request
+     * @param ResponseAbstract $response Response
+     * @param mixed            $data     Generic data
+     *
+     * @return RenderableInterface
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function viewProjectList(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/Modules/ResearchDevelopment/Theme/Backend/rnd-list');
+        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1002202001, $request, $response));
+
+        return $view;
+    }
+
+    /**
+     * @param RequestAbstract  $request  Request
+     * @param ResponseAbstract $response Response
+     * @param mixed            $data     Generic data
+     *
+     * @return RenderableInterface
+     *
+     * @since  1.0.0
+     * @author Dennis Eichhorn <d.eichhorn@oms.com>
+     */
+    public function viewProjectCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
+    {
+        $view = new View($this->app, $request, $response);
+        $view->setTemplate('/Modules/ResearchDevelopment/Theme/Backend/rnd-create');
+        $view->addData('nav', $this->app->moduleManager->get('Navigation')->createNavigationMid(1002202001, $request, $response));
+
+        return $view;
+    }
+
+}
