36264a17db377ca8108893a5b4acb27d98a725af
Dennis Eichhorn <spl1nes.com@googlemail.com>
2023-06-16 19:32:53 +0200

    Updated file header

--- Controller/Controller.php
+++ Controller/Controller.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Orange Management
+ * Karaka
  *
- * PHP Version 7.4
+ * PHP Version 8.1
  *
  * @package   Modules\ResearchDevelopment
  * @copyright Dennis Eichhorn
- * @license   OMS License 1.0
+ * @license   OMS License 2.0
  * @version   1.0.0
  * @link      https://orange-management.org
  */
@@ -21,7 +21,7 @@ use phpOMS\Module\WebInterface;
  * Research & Development controller class.
  *
  * @package Modules\ResearchDevelopment
- * @license OMS License 1.0
+ * @license OMS License 2.0
  * @link    https://orange-management.org
  * @since   1.0.0
  */
