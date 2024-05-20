bb40d3830814db49eb2e02e24117760e716c86a6
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:17 +0200

    fix version and bugs

--- Controller/BackendController.php
+++ Controller/BackendController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\ResearchDevelopment
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -23,7 +23,7 @@ use phpOMS\Views\View;
  * Research & Development controller class.
  *
  * @package Modules\ResearchDevelopment
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
--- Controller/Controller.php
+++ Controller/Controller.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\ResearchDevelopment
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -21,7 +21,7 @@ use phpOMS\Module\WebInterface;
  * Research & Development controller class.
  *
  * @package Modules\ResearchDevelopment
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
